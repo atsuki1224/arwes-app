998ea0d735e77decb40bd5c643620b8be16faeba
atsuki1224 <akeve1224@icloud.com>
2020-08-28 23:19:17 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ReactDOM from 'react-dom';
+import { ThemeProvider, createTheme, Arwes, Button } from 'arwes';
 
+const App = () => (
+  <ThemeProvider theme={createTheme()}>
+    <Arwes>
+      <h1>My App</h1>
+      <p>A SciFi Project</p>
+      <div style={{ padding: 20 }}>
+        <Button>My Button</Button>
+      </div>
+    </Arwes>
+  </ThemeProvider>
+);
 export default App;
+ReactDOM.render(<App />, document.querySelector('#root'));
