c805530fcf0fe636d517d3fe5adfd5a087d00282
atsuki1224 <akeve1224@icloud.com>
2020-08-29 01:05:19 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 import ReactDOM from 'react-dom';
-import { ThemeProvider, createTheme, Arwes, Button } from 'arwes';
-
+import { Logo, ThemeProvider,Loading,  createTheme,
+  SoundsProvider,Words, createLoader, createPlayer, createSounds, Link, Button, Arwes } from 'arwes';
 const App = () => (
   <ThemeProvider theme={createTheme()}>
-    <Arwes>
-      <h1>My App</h1>
-      <p>A SciFi Project</p>
-      <div style={{ padding: 20 }}>
-        <Button>My Button</Button>
-      </div>
+    <Arwes animate show>
+    <Logo animate size={300} />
+        <h3><Words animate>A cyberpunk UI project</Words></h3>
+        <p><Words animate>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
+        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
+        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
+        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
+        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
+        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
+        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
+        </Words></p>
+        <p><Words animate layer='success'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
+        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
+        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
+        </Words></p>
+        <p><Words animate layer='alert'>With animations based on SciFi and designs from high technology</Words></p>
+        <div style={{ margin: 20 }}>
+            This is an <Link href='https://github.com/atsuki1224'>Intergalactic Link</Link>.
+        </div>
+      <div>
+        <Loading animate />
+        <Loading animate small />
+        <div style={{ position: 'relative', width: 200, height: 200 }}>
+            <Loading animate full />
+        </div>
+    </div>
     </Arwes>
   </ThemeProvider>
 );
