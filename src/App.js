da25fb76228b9b520f86d0b07b21aead66d59862
atsuki1224 <akeve1224@icloud.com>
2020-08-29 15:15:17 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { ThemeProvider, createTheme, Arwes, Button, Header, Footer, Logo, Project, Words, Link } from 'arwes';
+
+const App = () => (
+  <ThemeProvider theme={createTheme()}>
+    <Arwes animate show>
+      {anim => (
+        <div style={{ padding: 20 }} class="content">
+          <Header animate>
+            <h1 style={{ margin: 0 }} class="header"> <Logo animate size={30} />　Atsuki Komuro</h1>
+          </Header>
+            <h1 class="title">WELL COME!</h1>
+            <div class="list">
+             <Link href='https://github.com/atsuki1224'> <Button animate show={anim.entered} >GitHub</Button></Link>
+             <Link href='https://twitter.com/akeve1224'> <Button animate show={anim.entered} >Twitter</Button></Link>
+             <Link href='https://medium.com/nextbeat-engineering'> <Button animate show={anim.entered} >Mediam</Button></Link>
+             <Link href='https://www.wantedly.com/users/129944770'> <Button animate show={anim.entered} >Wantedly</Button></Link>
+          </div>
+          <Project animate header='PROFILE' style={{padding: 130}}>
+            <p><Words animate show={anim.entered}>
+              今年の4月にネクストビートのエンジニアとして入社した小室と申します。
+            </Words>
+            <Words animate show={anim.entered}>
+              今回はARWESというフームワークを使用してReactで静的なページを作成してみました。
+            </Words></p>
+          </Project>
+            <Footer animate>
+              <Link href='https://github.com/arwes/arwes'><small class="footer">ARWES</small></Link>
+            </Footer>
+      </div>
+      )}
+    </Arwes>
+  </ThemeProvider>
+);
 
 export default App;
+ReactDOM.render(<App />, document.querySelector('#root'));
