4b0f4f9077a209def943d5559bacc6900b50ebb2
atsuki1224 <akeve1224@icloud.com>
2020-08-29 11:28:05 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Logo, ThemeProvider,Loading,  createTheme,
-  SoundsProvider,Words, createLoader, createPlayer, createSounds, Link, Button, Arwes } from 'arwes';
-const App = () => (
-  <ThemeProvider theme={createTheme()}>
-    <Arwes animate show>
-    <Logo animate size={300} />
-        <h3><Words animate>deploy test</Words></h3>
-        <p><Words animate>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
-        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
-        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
-        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
-        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
-        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
-        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
-        </Words></p>
-        <p><Words animate layer='success'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, amet
-        cupiditate laboriosam sunt libero aliquam, consequatur alias ducimus adipisci
-        nesciunt odit? Odio tenetur et itaque suscipit atque officiis debitis qui.
-        </Words></p>
-        <p><Words animate layer='alert'>With animations based on SciFi and designs from high technology</Words></p>
-        <div style={{ margin: 20 }}>
-            This is an <Link href='https://github.com/atsuki1224'>Intergalactic Link</Link>.
-        </div>
-      <div>
-        <Loading animate />
-        <Loading animate small />
-        <div style={{ position: 'relative', width: 200, height: 200 }}>
-            <Loading animate full />
-        </div>
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </Arwes>
-  </ThemeProvider>
-);
+  );
+}
+
 export default App;
-ReactDOM.render(<App />, document.querySelector('#root'));
